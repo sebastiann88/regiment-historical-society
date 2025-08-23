8c9b03654eb1a6ed82ad998c985be2b22e0ea548
sebastiann88 <sebastian.nadeau45@gmail.com>
2025-08-23 08:22:51 -0400

    feat(html, css, js): add gsap

--- js/scrollTrigger.js
+++ js/scrollTrigger.js
@@ -1,56 +1,42 @@
-// gsap.registerPlugin(ScrollTrigger, DrawSVGPlugin, MotionPathPlugin);
+// use a script tag or an external JS file
+ document.addEventListener("DOMContentLoaded", (event) => {
+  //learn how this was made at https://www.youtube.com/watch?v=ersN5fk8py0 
+  gsap.registerPlugin(ScrollTrigger, DrawSVGPlugin, MotionPathPlugin, GSDevTools);
 
-// gsap.defaults({ease: "none"});
+  gsap.defaults({ease: "none"});
 
-// const main = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: ".timeline",
-//     scrub: true,
-//     start: "top center",
-//     end: "bottom 20%",
-//     // markers: true
-//   }
-// })
-// .from(".theLine", {drawSVG: 0, duration: 1})
-// .to(".main-ball", {
-//   motionPath: {
-//     path: ".theLine",
-//     align: ".theLine",
-//     alignOrigin: [0.5, 0.5]
-//   },
-//   duration: 1
-// }, 0);
+  const pulses = gsap.timeline({
+    defaults: {
+      scale: 2,
+      autoAlpha:1,
+      transformOrigin: 'center', 
+      ease: "elastic(2.5, 1)"
+    }})
+  .to(".ball02, .text01", {}, 0.84) 
+  .to(".ball03, .text02", {}, 1.36)
+  .to(".ball04, .text03", {}, 1.92)
 
-// const pulses = gsap.timeline({
-//   defaults: {
-//     scale: 2,
-//     autoAlpha:1,
-//     transformOrigin: 'center', 
-//     ease: "elastic(2.5, 1)"
-//   }})
-// .to(".ball02, .text01", {}, 0.84) 
-// .to(".ball03, .text02", {}, 1.36)
-// .to(".ball04, .text03", {}, 1.92)
-
-// const main = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: "#svg",
-//     scrub: true,
-//     start: "top center",
-//     end: "bottom center"
-//   }
-// })
-// .to(".ball01", {autoAlpha:1, duration:0.05})
-// .from(".theLine", {drawSVG:0, duration:4}, 0)
-// .to(".ball01", {motionPath:{
-//   path:".theLine",
-//   align:".theLine",
-//   alignOrigin:[0.5, 0.5],
-// }, duration:4}, 0)
-// .add(pulses, 0)
+  const main = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#svg",
+      scrub: true,
+      start: "top center",
+      end: "bottom center",
+      // markers: true
+    }
+  })
+  .to(".mainball", {autoAlpha:1, duration:0.05})
+  .from(".theLine", {drawSVG:0, duration:4}, 0)
+  .to(".mainball", {motionPath:{
+    path:".theLine",
+    align:".theLine",
+    alignOrigin:[0.5, 0.5],
+  }, duration:4}, 0)
+  .add(pulses, 0)
 
 
-//GSDevTools.create({animation:main})
+  // GSDevTools.create({animation:main, minimal: false})
+ });
 
 /*const pulses = gsap.timeline({
   defaults: {
